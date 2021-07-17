51569b7abef4bc06b8afcfe38ed59defebeab280
denkaty <82085828+denkaty@users.noreply.github.com>
2021-07-17 22:00:23 +0300

    Add file

--- Sets and Maps Advanced/CitiesByContinentAndCountry_07.java
+++ Sets and Maps Advanced/CitiesByContinentAndCountry_07.java
@@ -1,10 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class CitiesByContinentAndCountry_07 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, Map<String, List<String>>> continentsMap = new LinkedHashMap<>();
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
 
+            String continent = tokens[0];
+            String country = tokens[1];
+            String city = tokens[2];
+
+            continentsMap.putIfAbsent(continent, new LinkedHashMap<>());
+            Map<String, List<String>> stringListMap = continentsMap.get(continent);
+            stringListMap.putIfAbsent(country, new ArrayList<>());
+            stringListMap.get(country).add(city);
+        }
+        continentsMap.entrySet()
+                .stream()
+                .forEach(continent -> {
+                    System.out.printf("%s:%n", continent.getKey());
+                    continent.getValue().forEach((country, city) -> {
+                        System.out.printf("%s -> %s%n", country, city.toString().replaceAll("[\\[\\]]", ""));
+                    });
+                });
     }
 }
