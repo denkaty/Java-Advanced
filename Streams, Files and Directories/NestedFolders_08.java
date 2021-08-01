2b389f810174d14f44fbee500f176a43652c3900
denkaty <82085828+denkaty@users.noreply.github.com>
2021-08-01 21:01:18 +0300

    Add file

--- Streams, Files and Directories/NestedFolders_08.java
+++ Streams, Files and Directories/NestedFolders_08.java
@@ -25,28 +25,5 @@ public class NestedFolders_08 {
 
         }
         System.out.println(counter + " folders");
-
-
-//        File file = new File("Files-and-Streams");
-//
-//        Deque<File> queue = new ArrayDeque<>();
-//        queue.offer(file);
-//        int counter = 1;
-//        while (!queue.isEmpty()) {
-//            File poll = queue.poll();
-//            System.out.println(poll.getName());
-//            File[] files = poll.listFiles();
-//
-//            for (File f : files) {
-//                if (f.isDirectory()) {
-//                    queue.offer(f);
-//                    counter++;
-//                } else {
-//                    System.out.println(f.getName());
-//                }
-//            }
-//
-//        }
-//        System.out.println(counter + " folders");
     }
 }
