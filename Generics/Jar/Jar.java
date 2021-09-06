863b362cebcd9866de8e6e6fcd49f6a6f88113d2
denkaty <82085828+denkaty@users.noreply.github.com>
2021-09-06 10:04:28 +0300

    Add file

---
+++ Generics/Jar/Jar.java
@@ -0,0 +1,22 @@
+package Jar;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Jar<Type> {
+    private Deque<Type> stack;
+
+    public Jar(){
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void add(Type element){
+        this.stack.push(element);
+    }
+
+    public Type remove(){
+        return this.stack.pop();
+    }
+
+
+}
