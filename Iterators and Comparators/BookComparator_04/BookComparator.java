bc972f1bc6456499fe99e99b06c4ac4d9c8a7619
denkaty <82085828+denkaty@users.noreply.github.com>
2021-09-21 19:38:42 +0300

    Add file

---
+++ Iterators and Comparators/BookComparator_04/BookComparator.java
@@ -0,0 +1,19 @@
+package BookComparator_04;
+
+import Book_01.Book;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        int result = o1.getTitle().compareTo(o2.getTitle());
+
+        if (result==0){
+            return Integer.compare(o1.getYear(), o2.getYear());
+        }
+        return result;
+    }
+}
